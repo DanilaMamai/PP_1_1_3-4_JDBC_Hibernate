ea02d1ce50ac2de01cc8b3fac8db73586638569f
Daniil Mamaenko <de_mamaenko@g.betboom.org>
2023-07-14 13:14:08 +0300

    fix/remove-jdbc -remove: jdbc methods

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,7 +1,5 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
@@ -16,15 +14,14 @@ public class Util {
 	private static final String URL = "jdbc:mysql://localhost:3306/test";
 	private static final String USER = "root";
 	private static final String PASS = "1234";
-	
+
 	private static SessionFactory sessionFactory;
-	private static Connection connection;
-	
+
 	public static SessionFactory getSessionFactory() {
 		if (sessionFactory == null) {
 			try {
 				Configuration configuration = new Configuration();
-				
+
 				Properties settings = new Properties();
 				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
 				settings.put(Environment.URL, URL);
@@ -34,13 +31,13 @@ public class Util {
 				settings.put(Environment.SHOW_SQL, true);
 				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 				settings.put(Environment.HBM2DDL_AUTO, "create");
-				
+
 				configuration.setProperties(settings);
 				configuration.addAnnotatedClass(User.class);
-				
-				ServiceRegistry sr = new StandardServiceRegistryBuilder()
-						.applySettings(configuration.getProperties()).build();
-				
+
+				ServiceRegistry sr = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+						.build();
+
 				sessionFactory = configuration.buildSessionFactory(sr);
 			} catch (Exception e) {
 				System.err.println("ERROR CONNECT DATABASE");
@@ -48,15 +45,5 @@ public class Util {
 		}
 
 		return sessionFactory;
-	}
-
-	public static Connection getConnection() {
-		try {
-			connection = DriverManager.getConnection(URL, USER, PASS);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		return connection;
 	}
 }
